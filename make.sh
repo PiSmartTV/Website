594e347e66b580cd8c186c82ded607c7a1fef389
Aleksa <alexa.ognjanovic@protonmail.com>
2020-09-25 21:53:56 +0200

    Added build script

---
+++ make.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+DIR="$(dirname "$0")"
+
+
+CSS_DIR="${DIR}/pitv/main/static/css"
+JS_DIR="${DIR}/pitv/main/static/js"
+IMG_DIR="${DIR}/pitv/main/static/images"
+
+SCSS_DIR="${DIR}/pitv/style/style.scss"
+JQUERY_JS="${DIR}/pitv/style/node_modules/jquery/dist/jquery.min.js"
+BOOTSTRAP_JS="${DIR}/pitv/style/node_modules/bootstrap/dist/js/bootstrap.min.js"
+
+mkdir ${CSS_DIR}
+mkdir ${JS_DIR}
+mkdir ${IMG_DIR}
+
+npm --prefix "${DIR}/pitv/style/" ci --save
+
+sass ${SCSS_DIR} "${CSS_DIR}/style.min.css" --style compressed
+cp ${JQUERY_JS} ${JS_DIR}
+cp ${BOOTSTRAP_JS} ${JS_DIR}
