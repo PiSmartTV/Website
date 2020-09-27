d472816944359d13655cadac3dae9d647e64794d
Aleksa <alexa.ognjanovic@protonmail.com>
2020-09-27 19:18:14 +0200

    Signup wired and working, signup errors left to add

--- pitv/main/forms.py
+++ pitv/main/forms.py
@@ -6,28 +6,22 @@ from django.utils.translation import gettext as _
 class CustomUserCreationForm(UserCreationForm):
     input_attrs = {'class' : 'form-control', 'placeholder': _('E-Mail Address')}
 
-    email = forms.EmailField(label="E-Mail", widget=forms.TextInput(attrs=input_attrs))
+    email = forms.EmailField(widget=forms.TextInput(attrs=input_attrs))
 
     input_attrs['placeholder'] = _('First Name')
-    first_name = forms.CharField(label="First Name", widget=forms.TextInput(attrs=input_attrs))
+    first_name = forms.CharField(widget=forms.TextInput(attrs=input_attrs))
 
     input_attrs['placeholder'] = _('Last Name')
-    last_name = forms.CharField(label="Last Name", widget=forms.TextInput(attrs=input_attrs))
+    last_name = forms.CharField(widget=forms.TextInput(attrs=input_attrs))
 
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': _('Password')})    
         self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': _('Confirm Password')})    
-
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': _('Username')})
 
     class Meta:
         model = User
         fields = ["first_name", "last_name", "username", "email", "password1", "password2"]
-        widgets = {
-            'username': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': _('Username')
-            }),
-        }
 
--- pitv/main/views.py
+++ pitv/main/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.utils.translation import gettext as _
+
 from .forms import CustomUserCreationForm
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'title': 'Home'})
 
 def signup(request):
-    form = CustomUserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('pitv-home')
+        else:
+            print(form.errors.as_text)
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form, 'title': _('Sign up')})
+
+    
