8b6c07e0e127f0c31dc9dad226dc07c418540fe8
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2021-04-09 18:07:32 +0200

    Setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+DIR="$(dirname "$0")"
+
+eval "${DIR}/manage.sh migrate"
+eval "${DIR}/manage.sh createsuperuser"
+eval "${DIR}/make.sh"
+eval "${DIR}/manage.sh collectstatic"
+eval "${DIR}/translate.sh"
